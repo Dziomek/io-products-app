b76ad86c5311f94e141bda65b0dcc13c696cab6a
Dziomek <dziomek@student.agh.edu.pl>
2022-11-20 16:17:16 +0100

    token (JWT) endpoint in the backend, requirements.txt update

--- flask_backend/flask_backend/__init__.py
+++ flask_backend/flask_backend/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
+
+app.config["JWT_SECRET_KEY"] = "randomowy-klucz"
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run()
--- flask_backend/flask_backend/views.py
+++ flask_backend/flask_backend/views.py
@@ -1,9 +1,22 @@
-import flask
+from flask import jsonify, request
 from . import app
+from flask_jwt_extended import create_access_token
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    response = flask.jsonify({"data": ["data1", "data2", "data3"]})
+    response = jsonify({"data": ["data1", "data2", "data3"]})
 
+    return response
+
+
+@app.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token": access_token}
     return response
