dcc7d21455206d5cc5f37fce537887aecf1a081c
Dziomek <dziomek@student.agh.edu.pl>
2022-11-23 14:11:52 +0100

    MySQL integration with Flask (SQLAlchemy), config with .env

--- flask_backend/flask_backend/__init__.py
+++ flask_backend/flask_backend/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from .models import db
 
 app = Flask(__name__)
 CORS(app)
-
-app.config["JWT_SECRET_KEY"] = "randomowy-klucz"
 jwt = JWTManager(app)
+app.config.from_prefixed_env()
+db.init_app(app)
+
+##with app.app_context():
+    ##db.create_all()
 
 if __name__ == '__main__':
     app.run()
