a4d85973f59d2776582ace579f0fdf278c3546d5
Marcin Krawiec <marcin.krawiec@protonmail.com>
2022-12-23 14:20:55 +0100

    Modify .gitignore

--- flask_backend/flask_backend/views.py
+++ flask_backend/flask_backend/views.py
@@ -1,15 +1,7 @@
-import hashlib
 import json
 import requests
 from flask import jsonify, request, url_for
-from flask_login import login_user, logout_user
 from . import app
-from . import email_verification
-from flask_jwt_extended import create_access_token
-from flask_bcrypt import generate_password_hash, check_password_hash
-from .utils import email_check, password_check, email_verification_token, verify_email, send_email_with_token
-from .email_verification import MailService
-from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from .database_connector import DatabaseConnector
 from scraping.scraping.spiders.ceneoScraping import ceneoScraping, zdrowieScraping, urodaScraping
 from scrapy.crawler import CrawlerProcess
