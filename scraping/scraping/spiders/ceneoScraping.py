63b4c122060ddcbd379f90fdb3d084f396040c5e
umilewska <ula.laniewska@gmail.com>
2022-12-30 15:53:37 +0100

    scraping with new functionalities

--- scraping/scraping/spiders/ceneoScraping.py
+++ scraping/scraping/spiders/ceneoScraping.py
@@ -1,10 +1,7 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-#wybór czy chcemy scrapować w kategorii uroda/ zdrowie czy obu
-#value = input("Please enter a category (uroda/zdrowie/obie):\n")
 
-#obie kategorie
 class ceneoScraping(scrapy.Spider):
     name = "ceneo_search"
 
@@ -14,94 +11,73 @@ class ceneoScraping(scrapy.Spider):
             keyword_list = []
         self.keyword_list = keyword_list
 
-    #przygotowywanie urli po których zaczniemy scrapowac - wyniki są posortowane od najniższej ceny
-    def start_requests(self):
-        #keyword_list = ['szminka', 'puder']
+    #keyword_list = ['szminka', 'puder']
+    #keyword_list = ['ibuprom zatoki sprint 10 kapsulek']
+    # keyword_list = ['ahgvkds']
+    def get_keyword_list(self):
         for keyword in self.keyword_list:
-            urls = [f"https://www.ceneo.pl/Uroda;szukaj-{keyword};0112-0.htm",
+            start_urls = [f"https://www.ceneo.pl/Uroda;szukaj-{keyword};0112-0.htm",
                     f"https://www.ceneo.pl/Zdrowie;szukaj-{keyword};0112-0.htm"]
-            for ceneo_serch_url in urls:
-                yield scrapy.Request(url=ceneo_serch_url, callback=self.parse_search_results, meta={'keyword': keyword})
+            self.start_urls = start_urls
 
-    #scrapowanie danych (nazwa, cena, link też będzie)
+    #przygotowywanie urli po których zaczniemy scrapowac - wyniki są posortowane od najniższej ceny
+    def parse(self, response, **kwargs):
+        urls = self.start_urls
+        for ceneo_serch_url in urls:
+            # niejednoznaczne wyszukanie
+            if len(response.css('div.cat-prod-row__content')) > 1:
+                yield scrapy.Request(url=ceneo_serch_url, callback=self.parse_search_results)
+            # jednoznaczne wyszukanie
+            elif len(response.css('div.cat-prod-row__content')) == 1:
+                try:
+                    link = 'https://www.ceneo.pl' + response.css('a.js_seoUrl.js_clickHash.go-to-product').attrib[
+                        'href'] + ';0284-0.htm'
+                    yield scrapy.Request(url=link, callback=self.parse_details)
+                except:
+                    link = 'https://www.ceneo.pl' + \
+                           response.css('a.cat-prod-row__product-link.js_clickHash.js_seoUrl.go-to-product').attrib[
+                               'href'] + '#tag=OneClickSearch'
+                    yield scrapy.Request(url=link, callback=self.parse_details)
+            # idk czy tutaj ten błąd jakoś pokazywać
+            else:
+                error = 'Nie znaleziono produktu'
+                print(error)
+
+
+    # scrapowanie danych dla niejednoznacznego wyszukania
     def parse_search_results(self, response):
-        for products in response.css('div.cat-prod-row__content'):
+        for products in response.css('div.cat-prod-row__content')[0:10]:
             product_name = products.css('span::text').get()
             price = products.css('span.value::text').get() + products.css('span.penny::text').get()
-            link = products.css('a.js_seoUrl.js_clickHash.go-to-product').attrib['href']
+            link = 'https://www.ceneo.pl' + products.css('a.js_seoUrl.js_clickHash.go-to-product').attrib['href']
             data = {
                 'name': product_name,
                 'price': price,
-                'link': 'https://www.ceneo.pl' + link
+                'link': link
             }
             yield data
-        next_page = response.css('a.pagination__item.pagination__next').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse_search_results)
 
-#kat uroda
-class urodaScraping(scrapy.Spider):
-    name = "uroda_serach"
-
-    def start_requests(self):
-        keyword_list = ['witamina c']
-        for keyword in keyword_list:
-            ceneo_serch_url = f"https://www.ceneo.pl/Uroda;szukaj-{keyword};0112-0.htm"
-            yield scrapy.Request(url=ceneo_serch_url, callback=self.parse_search_results, meta={'keyword': keyword})
-
-    def parse_search_results(self, response):
-
-        for products in response.css('div.cat-prod-row__content'):
-            product_name = products.css('span::text').get()
+    # scrapowanie danych dla jednoznacznego wyszukania
+    def parse_details(self, response):
+        data = {}
+        productName = response.css('h1.product-top__product-info__name.js_product-h1-link.js_product-force-scroll.js_searchInGoogleTooltip.default-cursor::text').get()
+        for products in response.css('div.product-offer__product.js_product-offer__product.js_productName.specific-variant-content')[0:1]:
             price = products.css('span.value::text').get() + products.css('span.penny::text').get()
-            link = products.css('a.js_seoUrl.js_clickHash.go-to-product').attrib['href']
-            data = {
-                'name': product_name,
-                'price': price,
-                'link': 'https://www.ceneo.pl' + link
-            }
-            yield data
-        next_page = response.css('a.pagination__item.pagination__next').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse_search_results)
+            data['name'] = productName
+            data['price'] = price
+        for supplier in response.css('div.product-offer__store')[0:1]:
+            shopName = supplier.css('img').attrib['alt']
+            data['shop name'] = shopName
+        for products_link in response.css('div.product-offer__actions.js_product-offer__actions.js_actions.specific-variant-content')[0:1]:
+            link = 'https://www.ceneo.pl/' + products_link.css('a.button.button--primary.button--flex.go-to-shop').attrib['href']
+            data['link'] = link
+        yield data
 
-#kategoria zdrowie
-class zdrowieScraping(scrapy.Spider):
-    name = "zdrowie_serach"
-
-    def start_requests(self):
-        keyword_list = ['witamina c']
-        for keyword in keyword_list:
-            ceneo_serch_url = f"https://www.ceneo.pl/Zdrowie;szukaj-{keyword};0112-0.htm"
-            yield scrapy.Request(url=ceneo_serch_url, callback=self.parse_search_results, meta={'keyword': keyword})
-
-    def parse_search_results(self, response):
-
-        for products in response.css('div.cat-prod-row__content'):
-            product_name = products.css('span::text').get()
-            price = products.css('span.value::text').get() + products.css('span.penny::text').get()
-            link = products.css('a.js_seoUrl.js_clickHash.go-to-product').attrib['href']
-            data = {
-                'name': product_name,
-                'price': price,
-                'link': 'https://www.ceneo.pl' + link
-            }
-            yield data
-        next_page = response.css('a.pagination__item.pagination__next').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse_search_results)
-
-#zapisywanie do pliku csv - da się do bazki ale to potem
+#zapisywanie do pliku csv
 # process = CrawlerProcess(settings={
 #     'FEED_URI': 'scraping.csv',
 #     'FEED_FORMAT': 'csv'
 # })
 
-#wybor spidera w zaleznosci od kategorii
-# if value=='uroda':
-#     process.crawl(urodaScraping)
-# elif value=='zdrowie':
-#     process.crawl(zdrowieScraping)
-# else:
-# process.crawl(ceneoScraping)
-# process.start() # the script will block here until the crawling is finished
+#process.crawl(ceneoScraping)
+#process.start() # the script will block here until the crawling is finished
