7699f6ee3ac223aef36924eaf4916f187d371b1e
Dziomek <dziomek@student.agh.edu.pl>
2023-01-05 11:18:09 +0100

    scraping functionality in home input

--- react_frontend/src/components/ProgressBar.js
+++ react_frontend/src/components/ProgressBar.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import '../css/ProgressBar.css'
 
 
-export default function Progressbar(props) {
+export default function ProgressBar(props) {
 	
 	// useEffect(() => {
 	// 	if (props.filled) setFilled(props.filled)
