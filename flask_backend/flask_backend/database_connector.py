f293d79ce5f49160ae5f02649378deb6d40fba1d
Marcin Krawiec <marcin.krawiec@protonmail.com>
2023-01-21 14:56:15 +0100

    Add database retention and db insterting endpoint

--- flask_backend/flask_backend/database_connector.py
+++ flask_backend/flask_backend/database_connector.py
@@ -13,8 +13,8 @@ class DatabaseConnector:
         sys.exit()
 
     @staticmethod
-    def insert_into_users(username, email, password_hash, is_active):
-        query = 'INSERT INTO users (username, email, password_hash, is_active) VALUES (%s, %s, %s, %s)', (username, email, password_hash, is_active)
+    def insert_into_users(username, email, password_hash, is_active, timestamp):
+        query = 'INSERT INTO users (username, email, password_hash, is_active, timestamp) VALUES (%s, %s, %s, %s, %s)', (username, email, password_hash, is_active, timestamp)
         print(query)
 
         try:
@@ -60,6 +60,49 @@ class DatabaseConnector:
         try:
             cursor = DatabaseConnector.database.cursor()
             cursor.execute(*query)
+            DatabaseConnector.database.commit()
+        except (mysql.connector.Error, AttributeError) as e:
+            logging.error('Query has not been executed: ' + str(e))
+
+    @staticmethod
+    def insert_into_products_history(user_id, name, link, price, timestamp):
+        query = 'INSERT INTO products_history (user_id, name, link, price, timestamp) VALUES (%d, %s, %s, %%.2f, %s)', (user_id, name, link, price, timestamp)
+        print(query)
+
+        try:
+            cursor = DatabaseConnector.database.cursor()
+            cursor.execute(*query)
+            DatabaseConnector.database.commit()
+        except (mysql.connector.Error, AttributeError) as e:
+            logging.error('Query has not been executed: ' + str(e))
+
+    @staticmethod
+    def select_from_products_history(user_id):
+        query = 'SELECT * FROM products_history WHERE user_id=%d', (user_id,)
+        print(query)
+        try:
+            cursor = DatabaseConnector.database.cursor()
+            cursor.execute(*query)
+            data = []
+            for element in cursor:
+                data.append(element)
+            return data
+        except (mysql.connector.Error, AttributeError) as e:
+            logging.error('Query has not been executed: ' + str(e))
+
+    @staticmethod
+    def execute_database_retention():
+        query_1 = 'DELETE FROM products_history WHERE timestamp < now() - INTERVAL 6 MONTH'
+        print(query_1)
+        #query_2 = 'DELETE FROM users WHERE timestamp < now() - INTERVAL 6 MONTH'
+        #print(query_2)
+
+        try:
+            cursor = DatabaseConnector.database.cursor()
+            cursor.execute('SET SQL_SAFE_UPDATES = 0')
+            cursor.execute(*query_1)
+            #cursor.execute(*query_2)
+            cursor.execute('SET SQL_SAFE_UPDATES = 1')
             DatabaseConnector.database.commit()
         except (mysql.connector.Error, AttributeError) as e:
             logging.error('Query has not been executed: ' + str(e))
--- flask_backend/flask_backend/views.py
+++ flask_backend/flask_backend/views.py
@@ -1,6 +1,8 @@
 import json
+from datetime import datetime
+
 import requests
-from flask import jsonify, request, url_for
+from flask import jsonify, request, url_for, session
 from . import app
 from .database_connector import DatabaseConnector
 # from scraping.scraping.spiders.ceneoScraping import ceneoScraping, zdrowieScraping, urodaScraping
@@ -17,12 +19,14 @@ def hello_world():
 @app.route('/scraping', methods=['POST'])
 def scraping():
     product_list = request.json.get("productList")
+    category = request.json.get("category")
     quantity = request.json.get("quantity")
     
-    print(type(product_list))
+    #print(type(product_list))
 
     crawl_args = {
         "keyword_list": product_list,
+        "category": category,
         "quantity": quantity
     }
 
@@ -41,4 +45,21 @@ def scraping():
         "message": "Keyword list passed successfully",
         "product_list": data,
         "crawl_args_json": crawl_args_json
+    }
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    if session['logged_in'] == True:
+        data = request.json.get("data")
+        print(data)
+        #TODO: Process the data
+        now = datetime.now()
+        #db.insert_into_products_history(user_id=session['id'], name='', link='', price=0.00, timestamp=now.strftime("%Y-%m-%d %H:%M:%S"))
+        return {
+            "message": "Successfully inserted products into database"
+        }
+
+    return {
+        "message": "Skipping database insert as user is not logged in"
     }
